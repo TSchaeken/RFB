3131d03afa14ecbf477c02713b6a428e4fdfb59c
Tim Schaeken <tschaeken@gmail.com>
2018-11-19 17:12:30 -0700

    Videos 7-9, helper functions, SFC, and proptypes

--- Catch-Of-The-Day/src/components/StorePicker.js
+++ Catch-Of-The-Day/src/components/StorePicker.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { format } from 'util';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
   render() {
@@ -9,7 +9,12 @@ class StorePicker extends Component {
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
